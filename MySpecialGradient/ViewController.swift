405b4f18b9c1e252404afd41dae42f0bba4a8e49
Gain Chang <gainchang620@gmail.com>
2020-04-05 03:29:37 +0900

    Init gradient project

--- MySpecialGradient/ViewController.swift
+++ MySpecialGradient/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = self.view.bounds
+//        gradientLayer.colors = [UIColor.blue.cgColor, UIColor.white.cgColor]
+//        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
 
