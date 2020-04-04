a2c0b583d51049d501a5e48223e0e9e15c689f61
Gain Chang <gainchang620@gmail.com>
2020-04-05 02:19:04 +0900

    Initial Commit

---
+++ MySpecialGradient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySpecialGradient
+//
+//  Created by Gain Chang on 2020/04/05.
+//  Copyright © 2020 Gain Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
